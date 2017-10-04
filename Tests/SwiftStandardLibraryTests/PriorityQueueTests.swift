d5b2c8f252813693fd40f1e3447f30f7911bf8ef
Tyler Kirtland <tyler.kirtland@krush.com>
2017-10-04 17:47:05 -0400

    Created Priority Queue

---
+++ Tests/SwiftStandardLibraryTests/PriorityQueueTests.swift
@@ -0,0 +1,15 @@
+//
+//  PriorityQueueTests.swift
+//  SwiftStandardLibraryTests
+//
+//  Created by Tyler Kirtland on 10/2/17.
+//
+
+import XCTest
+@testable import SwiftStandardLibrary
+
+class PriorityQueueTests: XCTestCase {
+    
+    
+    
+}
